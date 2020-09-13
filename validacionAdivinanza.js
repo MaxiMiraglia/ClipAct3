5738bf72edb9226e19597296a36b82b65203edcc
Maxi Miraglia <maxi_miraglia@hotmail.com>
2020-09-13 20:46:09 -0300

    Ultimas correcciones

---
+++ validacionAdivinanza.js
@@ -0,0 +1,37 @@
+var intentos = 1;
+function adivinanza() {
+    var rtacorrecta = "estrella de neutrones";
+    var rtainput = document.getElementById("rta").value;
+    
+    if (rtainput.trim() === "") {
+        alert("No puede dejar el campo vacío.");
+        return false;
+    }
+    else {
+        if (rtainput.trim().toLowerCase() === rtacorrecta) {
+            alert("¡Respuesta correcta! Adivinaste en " + intentos + " intentos");
+            alert("Se ha desactivado el boton de envío, para volver a jugar resfresque la página.")
+            document.getElementById("enviar").disabled = true;
+        }
+        else if (intentos === 1) {
+            alert("Respuesta incorrecta. Intentos realizados: " + intentos);
+            intentos++;
+        }
+        else if (intentos === 2) {
+            alert("Respuesta incorrecta. Intentos realizados: " + intentos);
+            alert("Pista 1: es una estrella");
+            intentos++;
+        }
+        else if (intentos === 3) {
+            alert("Respuesta incorrecta. Intentos realizados: " + intentos);
+            alert("Pista 2: está compuesta mayormente por neutrones...");
+            intentos++;
+        }
+        else if (intentos === 4) {
+            alert("Respuesta incorrecta. Gastaste tu último intento. Intentos realizados: " + intentos)
+            alert("La respuesta era: estrella de neutrones");
+            alert("Se ha desactivado el boton de envío, para volver a jugar resfresque la página.");
+            document.getElementById("enviar").disabled = true;
+        }
+    }
+}
