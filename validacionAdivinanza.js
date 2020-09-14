2bf7641aec921ac5545508ca553bb559e78777a7
Maxi Miraglia <maxi_miraglia@hotmail.com>
2020-09-13 21:24:16 -0300

    Ultima correccion al script

--- validacionAdivinanza.js
+++ validacionAdivinanza.js
@@ -12,26 +12,31 @@ function adivinanza() {
             alert("¡Respuesta correcta! Adivinaste en " + intentos + " intentos");
             alert("Se ha desactivado el boton de envío, para volver a jugar resfresque la página.")
             document.getElementById("enviar").disabled = true;
+            return false;
         }
         else if (intentos === 1) {
             alert("Respuesta incorrecta. Intentos realizados: " + intentos);
             intentos++;
+            return false;
         }
         else if (intentos === 2) {
             alert("Respuesta incorrecta. Intentos realizados: " + intentos);
             alert("Pista 1: es una estrella");
             intentos++;
+            return false;
         }
         else if (intentos === 3) {
             alert("Respuesta incorrecta. Intentos realizados: " + intentos);
             alert("Pista 2: está compuesta mayormente por neutrones...");
             intentos++;
+            return false;
         }
         else if (intentos === 4) {
             alert("Respuesta incorrecta. Gastaste tu último intento. Intentos realizados: " + intentos)
             alert("La respuesta era: estrella de neutrones");
             alert("Se ha desactivado el boton de envío, para volver a jugar resfresque la página.");
             document.getElementById("enviar").disabled = true;
+            return false;
         }
     }
 }
